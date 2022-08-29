c63e3726d0c281d84bbf1331c96f8a2bbf3f0592
Kevin Old <kevin@kevinold.com>
2022-08-29 15:55:19 -0500

    wip - configure auth0-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useAuth0 } from "@auth0/auth0-react";
+import { Amplify } from "aws-amplify";
+import React from "react";
 import './App.css';
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports)
+
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <button onClick={() => logout({ returnTo: window.location.origin })}>
+      Log Out
+    </button>
+  );
+};
+
+const Profile = () => {
+  const { user, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+  );
+};
+
+/*
+// auth0 configuration, more info in: https://auth0.com/docs/libraries/auth0js/v9#available-parameters
+Auth.configure({
+    auth0: {
+        domain: 'your auth0 domain', 
+        clientID: 'your client id',
+        redirectUri: 'your call back url',
+        audience: 'https://your_domain/userinfo',
+        responseType: 'token id_token', // for now we only support implicit grant flow
+        scope: 'openid profile email', // the scope used by your app
+        returnTo: 'your sign out url'
+    }
+});
+*/
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
+  if (!isAuthenticated) return <div className="App"><LoginButton /></div>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile />
+      <LogoutButton />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+import { Auth0Provider } from "@auth0/auth0-react";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Auth0Provider
+    domain="dev-kevold-amz.us.auth0.com"
+    clientId="bSdJVwMcFJEZtN1UXK5ChmHL3UTqjCP3"
+    redirectUri={window.location.origin}
+  >
     <App />
+  </Auth0Provider>
   </React.StrictMode>
 );
 
