25984a8d3aed3acda46dd291838c24d7c4e511f2
Kevin Old <kevin@kevinold.com>
2022-10-05 08:10:06 -0500

    add auth with oidc

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,17 @@
 export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "202208291117amplifyc0b27aef9": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
     "api": {
         "202208291117amplifyc": {
             "GraphQLAPIIdOutput": "string",
